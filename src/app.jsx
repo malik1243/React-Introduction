ac3d1d147d6310a198e54413b13f55566a3598a5
malik1243 <muhammadali.84123@gmail.com>
2024-09-03 22:40:18 +0500

    react

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const app = () => {
+  return (
+    <>
+    <h1>hello world</h1>
+    <p>this is Muhammad Ali.And this is my first ever code of react by using vite library</p>
+    </>
+  )
+}
+
+export default app
